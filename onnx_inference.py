1800884f0390541bf3fe41259d9a78e22f081a9a
Shubham Kumar <skumar@monarchtractor.com>
2024-06-16 00:02:12 +0530

    onnx_inference -> final refactoring

--- onnx_inference.py
+++ onnx_inference.py
@@ -10,6 +10,7 @@ import os
 import random
 import re
 import time
+from tqdm import tqdm
 
 # TO-DO -> 
 # - use np.squeeze()
@@ -43,7 +44,7 @@ def inference(left_img, right_img, model, model_no_flow, img_dims=(480, 640)):
 	imgL = np.ascontiguousarray(imgL.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
 	imgR = np.ascontiguousarray(imgR.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
 
-	logging.warn(f"imgL.shape: {imgL.shape} imgR.shape: {imgR.shape}")
+	# logging.warn(f"imgL.shape: {imgL.shape} imgR.shape: {imgR.shape}")
 
 	imgL_dw2 = cv2.resize(left_img, (w // 2, h // 2), interpolation=cv2.INTER_LINEAR)
 	imgR_dw2 = cv2.resize(right_img, (w//2, h//2),  interpolation=cv2.INTER_LINEAR)
@@ -79,17 +80,14 @@ def inference(left_img, right_img, model, model_no_flow, img_dims=(480, 640)):
 # (H, W)
 DIMS = (480, 640)
 H,W = DIMS
-
 ZED_IMAGE_DIR = "zed_input/images"
 
-if __name__ == "__main__": 
+
 	
-	coloredlogs.install(level="WARN", force=True)  # install a handler on the root logger
-	
-	# setting up cv2 window
+def main(num_frames):
 	cv2.namedWindow("TEST", cv2.WINDOW_NORMAL)
 	cv2.resizeWindow("TEST", 2 * W, H)
-	# load the onnx models
+
 	sess_crestereo = ort.InferenceSession('models/crestereo.onnx')
 	sess_crestereo_no_flow = ort.InferenceSession('models/crestereo_without_flow.onnx')
 
@@ -102,68 +100,58 @@ if __name__ == "__main__":
 	image_files_right.sort()
 
 
-	logging.warn(f"image_files_left: {image_files_left[:5]}")
-	logging.warn(f"image_files_right: {image_files_right[:5]}")
+	# logging.warn(f"image_files_left: {image_files_left[:5]}")
+	# logging.warn(f"image_files_right: {image_files_right[:5]}")
 	assert(len(image_files_left) == len(image_files_right)), "Number of left and right images should be equal"
-	
+	assert(len(image_files_left) > num_frames), "Number of frames should be less than total number of images"
 	frame_rates = []
 	
-	num_images = len(image_files_left)
-	for i in range(0, min(num_images, 10)):
-		rand_idx = random.randint(0, num_images - 1)
-		
+	# generating random frame indices
+	frame_indices = random.sample(range(0, len(image_files_left) - 1), num_frames)
+
+	for i in tqdm(frame_indices):
+		rand_idx = random.randint(0, num_frames - 1)
+		# logging.warn(f"rand_idx: {rand_idx}")
 		left_img = cv2.imread(image_files_left[rand_idx])
 		right_img = cv2.imread(image_files_right[rand_idx])
-		
-		# right_img = cv2.imread(image_files[rand_idx // 2 + 1])
-		
-		# logging.warning(f"left_img: {left_img}"
 
 		left = cv2.resize(left_img, (W, H), interpolation=cv2.INTER_LINEAR)
 		right = cv2.resize(right_img, (W, H), interpolation=cv2.INTER_LINEAR)
 
 		start_time = time.time()
+		
 		model_inference = inference(left_img , right_img, sess_crestereo, sess_crestereo_no_flow, img_dims=(480, 640))   
-		logging.warning(f"model_inference.shape: {model_inference.shape} model_inference.dtype: {model_inference.dtype}") 
+		# logging.warning(f"model_inference.shape: {model_inference.shape} model_inference.dtype: {model_inference.dtype}") 
 		model_inference_depth_map_mono = utils.uint8_normalization(model_inference)
-		logging.warning(f"model_inference_depth_map_mono.shape: {model_inference_depth_map_mono.shape} model_inference.dtype: {model_inference_depth_map_mono.dtype}") 
+		# logging.warning(f"model_inference_depth_map_mono.shape: {model_inference_depth_map_mono.shape} model_inference.dtype: {model_inference_depth_map_mono.dtype}") 
 		model_infereence_depth_map_unit8 = cv2.cvtColor(model_inference_depth_map_mono, cv2.COLOR_GRAY2BGR)
 
+		end_time = time.time()
+		inference_time = end_time - start_time
+		frame_rate = 1 / inference_time
+		frame_rates.append(frame_rate)
 
+		# logging.warn()
 		# visualizing the results
 		# cv2.imshow("TEST", left)
 		# cv2.waitKey(0)
 		# cv2.imshow("TEST", model_infereence_depth_map_unit8)
 		# cv2.waitKey(0)
 		cv2.imshow("TEST", cv2.hconcat([left, model_infereence_depth_map_unit8]))
-		cv2.waitKey(0)
-
-		
-
+		cv2.waitKey(0)	
 	
-	# # Load and preprocess your images
-	# # for image in image_files:
-	# # 	# left_img = cv2.imread('zed_input/images/left_20.png')
-	# # 	# right_img = cv2.imread('zed_input/images/right_20.png')
-
-	# # 	left = cv2.resize(left_img, (W, H), interpolation=cv2.INTER_LINEAR)
-	# 	right = cv2.resize(right_img, (W, H), interpolation=cv2.INTER_LINEAR)
-
-	# 	model_inference = inference(left_img , right_img, sess_crestereo, sess_crestereo_no_flow, img_dims=(480, 640))   
-	# 	logging.warning(f"model_inference.shape: {model_inference.shape} model_inference.dtype: {model_inference.dtype}") 
-	# 	model_inference_depth_map_mono = utils.uint8_normalization(model_inference)
-	# 	logging.warning(f"model_inference_depth_map_mono.shape: {model_inference_depth_map_mono.shape} model_inference.dtype: {model_inference_depth_map_mono.dtype}") 
-	# 	model_infereence_depth_map_unit8 = cv2.cvtColor(model_inference_depth_map_mono, cv2.COLOR_GRAY2BGR)
-
-
-	# 	# visualizing the results
-	# 	cv2.imshow("TEST", left)
-	# 	cv2.waitKey(0)
-	# 	cv2.imshow("TEST", model_infereence_depth_map_unit8)
-	# 	cv2.waitKey(0)
-	# 	cv2.imshow("TEST", cv2.hconcat([left, model_infereence_depth_map_unit8]))
-	# 	cv2.waitKey(0)
+	plt.plot(frame_rates)
+	plt.xlabel('Image index')
+	plt.ylabel('Frame rate (frames per second)')
+	plt.title('ONNX model inference frame rate')
+	plt.show()
 
 	cv2.destroyAllWindows()
 
+
+if __name__ == "__main__": 
 	
+	coloredlogs.install(level="WARN", force=True)  # install a handler on the root logger
+	
+	num_frames = 5
+	main(num_frames)	
