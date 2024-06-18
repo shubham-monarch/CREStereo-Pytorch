72a29d00b0c54f71702cf8bf92aab3963cc4f069
Shubham Kumar <skumar@monarchtractor.com>
2024-06-18 08:12:09 +0530

    combined flow/no-flow model -> inference is bad

--- trt_inference.py
+++ trt_inference.py
@@ -16,7 +16,6 @@ import matplotlib.pyplot as plt
 
 
 # TO-DO
-# - check tensort version compatibility
 # - using polygraphy for quick onnx model checking
 # - refer official nvidia docs for best practices
 # - explore onnx-graphsurgeon and polygraphy 
@@ -28,27 +27,22 @@ import matplotlib.pyplot as plt
 # - explore FPS() class -> YOLO -> https://github.com/sithu31296/PyTorch-ONNX-TRT/blob/master/examples/yolov4/yolov4/scripts/infer.py
 # - package structure from YOLO 
 # - add trt_info functions -> https://github.com/NVIDIA/TensorRT/tree/main/tools/experimental/trt-engine-explorer#workflow
-# - add more attributes to the engine configuration -> optimization profile, f16, explore config.set_flag()
 # - add pre / post processing
-# - make onxx_without_flow work without onnx-simplifier
 # - addded batch_inference
 # - refactor code based on official nvidia examples
-# - to use ravel
-# - upgrade HostDeviceMem class -> https://github.com/NVIDIA/TensorRT/blob/main/samples/python/common_runtime.py
 # - add cuda loading in .ppth to .onnx conversion script
 # - reload output to torch for further processing
-# - fix negative values in flow_init
-# - improve output normalization
+# - fix negative values in flow_init **
+# - improve output normalization **
 # - refactor TRTEngine
+# - explore dynamic shapess
+# - altenative to using two trt engines
 
-# (H, W)
-DIMS = (480, 640)
-H,W = DIMS
-ZED_IMAGE_DIR = "zed_input/images"
 
 TRT_LOGGER = trt.Logger(trt.Logger.INFO)
 EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
 runtime = trt.Runtime(TRT_LOGGER)
+ZED_IMAGE_DIR = "zed_input/images"
 
 
 class TRTEngine:
@@ -130,8 +124,8 @@ class TRTEngine:
 			# output = np.reshape(data, (1, 2, 640, 640))[0]
 			return trt_inference_outputs
 
-	def log_engine_io_details(self):
-		logging.warning(f"[INSPECTING TRT_ENGINE.inputs/outputs]")
+	def log_engine_io_details(self, engine_name):
+		logging.warning(f"[INSPECTING {engine_name}.inputs/outputs] ==> ")
 		for input in self.inputs:
 			name = input['name']
 			logging.warning(f"{name}.shape: {input['shape']} {name}.dtype: {input['dtype']}") 
@@ -139,73 +133,123 @@ class TRTEngine:
 		for output in self.outputs: 
 			name = output['name']
 			logging.warning(f"{name}.shape: {output['shape']} {name}.dtype: {output['dtype']}")
-	
-
+		logging.warning(f"\n")
 
 
 
 def main():
+	# managing cv2 window
 	cv2.namedWindow("TEST", cv2.WINDOW_NORMAL)
-	cv2.resizeWindow("TEST", (W, H))
-
-	# plt.figure(figsize=(10,10))
+	cv2.resizeWindow("TEST", (640, 480))
+	
+	# managing plt datasets
 	plt_datasets = []
 
-	logging.debug(f"TensortRT version: {trt.__version__}")
-	
+	# onnx model paths
 	path_onnx_model = "models/crestereo.onnx"
-	path_trt_engine = path_onnx_model.replace(".onnx", ".trt")	
-	
 	path_onnx_model_without_flow = "models/crestereo_without_flow.onnx"
+	
+	# trt engine paths
+	path_trt_engine = path_onnx_model.replace(".onnx", ".trt")		
 	path_trt_engine_without_flow = path_onnx_model_without_flow.replace(".onnx", ".trt")
 
-	trt_engine = TRTEngine(path_trt_engine)
-	trt_engine.log_engine_io_details()
-
-	# trt_engine_without_flow = TRTEngine(path_trt_engine_without_flow)
-	# trt_engine_without_flow.log_engine_io_details("TRT_ENGINE_WITHOUT_FLOW")
-
-	# PREPARING INPUT DATA
-	# left_img = cv2.imread(f"{ZED_IMAGE_DIR}/left_18.png")
-	# right_img = cv2.imread(f"{ZED_IMAGE_DIR}/right_18.png")
-
-	# (w ,h) = (W, H)
-	# # imgL_dw2 = cv2.resize(left_img, (w // 2, h // 2), interpolation=cv2.INTER_LINEAR)
-	# # imgR_dw2 = cv2.resize(right_img, (w//2, h//2),  interpolation=cv2.INTER_LINEAR)
-	# # flow_init = np.random.random_sample((1, 2, h//2, w//2)).astype(np.float32)
-	# # flow_init = np.zeros((1, 2, h//2, w//2)).astype(np.float32)
-	# imgL = cv2.resize(left_img, (w, h), interpolation=cv2.INTER_LINEAR)
-	# imgR = cv2.resize(right_img, (w, h), interpolation=cv2.INTER_LINEAR)
-	# # imgL = cv2.resize(left_img, (h, w), interpolation=cv2.INTER_LINEAR)
-	# # imgR = cv2.resize(right_img, (h, w), interpolation=cv2.INTER_LINEAR)
-	# flow_init = np.random.randn(1, 2, h//2, w//2).astype(np.float32) * 255.0
-	# # flow_init = np.zeros((1, 2, h//2, w//2)).astype(np.float32)
 	
-	# # imgL_dw2 = np.ascontiguousarray(imgL_dw2.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32) 
-	# # imgR_dw2 = np.ascontiguousarray(imgR_dw2.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
-	# imgL = np.ascontiguousarray(imgL.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
-	# imgR = np.ascontiguousarray(imgR.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
-	# flow_init = np.ascontiguousarray(flow_init)
+	# IMG DIMS
+	(H, W) = (480, 640)
+
+	# READING IMAGES FROM DISK
+	left_img = cv2.imread(f"{ZED_IMAGE_DIR}/left_18.png")
+	right_img = cv2.imread(f"{ZED_IMAGE_DIR}/right_18.png")
+
+	# < --------------- TRT_ENGINE_WITHOUT_FLOW ------------------- >
+	# engine construction
+	trt_engine_without_flow = TRTEngine(path_trt_engine_without_flow)
+	trt_engine_without_flow.log_engine_io_details(engine_name="TRT_ENGINE_WITHOUT_FLOW")
+	
+	# pre-processing input
+	imgL_dw2 = cv2.resize(left_img, (W // 2, H // 2), interpolation=cv2.INTER_LINEAR)
+	imgR_dw2 = cv2.resize(right_img, (W //2, H //2),  interpolation=cv2.INTER_LINEAR)
+	
+	imgL_dw2 = np.ascontiguousarray(imgL_dw2.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32) 
+	imgR_dw2 = np.ascontiguousarray(imgR_dw2.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
+	
+	# loading input
+	trt_engine_without_flow.inputs[0]['host'] = imgL_dw2
+	trt_engine_without_flow.inputs[1]['host'] = imgR_dw2
+	
+	# inference
+	trt_inference_outputs =  trt_engine_without_flow.run_trt_inference()
+	logging.info(f"len(trt_inference_outputs): {len(trt_inference_outputs)}")
+	logging.info(f"trt_inference_outputs[0].shape: {trt_inference_outputs[0].shape} trt_inference_outputs[0].dtype: {trt_inference_outputs[0].dtype}")
+
+	# resizing outputs
+	trt_output = trt_inference_outputs[0].reshape(1, 2, H // 2, W // 2)
+	trt_output_squeezed = np.squeeze(trt_output[:, 0, :, :])
+	trt_output_uint8 = utils.uint8_normalization(trt_output_squeezed)	
+	cv2.imshow("TEST", trt_output_uint8)
+	cv2.waitKey(0)	
+
+	logging.warning(f"output_uint8.shape: {trt_output_uint8.shape} output_uint8.dtype: {trt_output_uint8.dtype}")
+
+	# < --------------- TRT_ENGINE ------------------- >
+	# engine construction
+	trt_engine = TRTEngine(path_trt_engine)
+	trt_engine.log_engine_io_details(engine_name="TRT_ENGINE")
+	
+	# pre-processing input
+	imgL = cv2.resize(left_img, (H, W), interpolation=cv2.INTER_LINEAR)
+	imgR = cv2.resize(right_img, (H, W), interpolation=cv2.INTER_LINEAR)
+	flow_init = trt_output
+	
+	imgL = np.ascontiguousarray(imgL.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
+	imgR = np.ascontiguousarray(imgR.transpose(2, 0, 1)[None, :, :, :]).astype(np.float32)
+	flow_init = np.ascontiguousarray(flow_init)
+
+	# loading input	
+	trt_engine.inputs[0]['host'] = imgL
+	trt_engine.inputs[1]['host'] = imgR
+	trt_engine.inputs[2]['host'] = flow_init
+
+	# inference
+	trt_inference_outputs =  trt_engine.run_trt_inference()
+	logging.info(f"len(trt_inference_outputs): {len(trt_inference_outputs)}")
+	logging.info(f"trt_inference_outputs[0].shape: {trt_inference_outputs[0].shape} trt_inference_outputs[0].dtype: {trt_inference_outputs[0].dtype}")
+
+	# resizing outputs
+	trt_output = trt_inference_outputs[0].reshape(1, 2, H, W)
+	
+	trt_output_squeezed = np.squeeze(trt_output[:, 0, :, :])
+	trt_output_uint8 = utils.uint8_normalization(trt_output_squeezed)
+
+	logging.warning(f"output_uint8.shape: {trt_output_uint8.shape} output_uint8.dtype: {trt_output_uint8.dtype}")
+
+	cv2.imshow("TEST", trt_output_uint8)
+	cv2.waitKey(0)
+
+	# # flattened_data_float32 = output.flatten()
+	# # logging.warning(f"flattened_data_float32.min: {output.flatten().min()} flattened_data_float32.max: {output.flatten().max()}")
+	# # plt.subplot(1,2,1)
+	# logging.warning(f"[BEFORE OUTLIER FILTERING]output.shape: {output.shape} output.dtype: {output.dtype}")
+	# output_outlier_filtered = utils.reject_outliers_2(output)
+	# logging.warning(f"[AFTER OUTLIER FILTERING] output_outlier_filtered.shape: {output_outlier_filtered.shape} output_outlier_filtered.dtype: {output_outlier_filtered.dtype}")	
+
+	# # utils.plot_histogram(output_outlier_filtered, 'output_outlier_filtered', bins=20, range=[0,1])
+	# plt_datasets.append([output_outlier_filtered, 'output_outlier_filtered', 20, [0.,1.]])
+	# output_uint8 = utils.uint8_normalization(output_outlier_filtered)
+	# output_uint8 = np.where(output_uint8 != 0, output_uint8, np.nan)
+	# logging.warning(f"output_uint8.shape: {output_uint8.shape} output_uint8.dtype: {output_uint8.dtype}")
+
+	# plt_datasets.append([output_uint8, 'output_uint8', 255, [0,255]])
+	
+	# utils.plot_histograms(plt_datasets)
+
+
 
 	# logging.debug("[INPUT DATA FINAL SHAPES] => ")
 	# logging.warning(f"imgL.shape: {imgL.shape}")
 	# logging.warning(f"imgR.shape: {imgR.shape}")
 	# logging.warning(f"flow_init.shape: {flow_init.shape}")
 
-	# # VISUALIZING INPUT DATA
-	# left_image_cv = np.squeeze(imgL[:, :, :, :])
-	# left_image_cv = utils.uint8_normalization(left_image_cv).transpose(1, 2, 0)
-	
-	# right_image_cv = np.squeeze(imgR[:, :, :, :])
-	# right_image_cv = utils.uint8_normalization(right_image_cv).transpose(1, 2, 0)
-	
-	# logging.warning(f"left_image_cv.shape: {left_image_cv.shape} left_image_cv.dtype: {left_image_cv.dtype}")
-	# logging.warning(f"right_image_cv.shape: {right_image_cv.shape} right_image_cv.dtype: {right_image_cv.dtype}")
-
-	
-	# cv2.imshow("TEST", cv2.hconcat([left_image_cv, right_image_cv]))
-	# cv2.waitKey(0)
-	
 	
 
 	# # LOADING PREPARED INPUT DATA TO TRT ENGINE 
@@ -215,9 +259,6 @@ def main():
 	# trt_engine.inputs[1]['host'] = imgR
 	# trt_engine.inputs[2]['host'] = flow_init
 
-	# # trt_engine.inputs[0]['host'] = np.ravel(imgL_dw2)
-	# # trt_engine.inputs[1]['host'] = np.ravel(imgR_dw2)
-	# # trt_engine.inputs[2]['host'] = np.ravel(flow_init)
 	
 	
 	# # RUNNING INFERENCE
@@ -252,5 +293,6 @@ def main():
 	# plt.close()
 	
 if __name__ == '__main__':
-	coloredlogs.install(level="WARN", force=True)  # install a handler on the root logger
+	coloredlogs.install(level="INFO", force=True)  # install a handler on the root logger
+	logging.debug(f"TensortRT version: {trt.__version__}")
 	main()
